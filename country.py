c51ad72abf5a8a6512f70a12d8502f34d9ad9748
Ubuntu <ubuntu@ip-172-31-43-207.us-east-2.compute.internal>
2021-01-13 15:06:16 +0000

    second commit

--- country.py
+++ country.py
@@ -2,17 +2,21 @@ import json
 import requests
 import subprocess
 base_url = "https://freegeoip.app/json/"
-i = open("result.txt")
+inn = open("result.txt", "w+")
 subprocess.call("/home/ubuntu/gather.sh")
 
 
 print("*******************************Lets start with countries************************************")
 
-for i in i:
+for i in inn:
 	url = base_url + i.strip()
 	r = requests.get(url)
 	jsonres = r.json()
 	print("*******************************")
 	print(" " + jsonres["country_name"])
+	if (len(jsonres["city"]) == 0):
+		print(" City not found")
+	else:
+		print(" " + jsonres["city"])
 	print(i)
 
